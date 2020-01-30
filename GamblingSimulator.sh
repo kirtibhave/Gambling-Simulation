e88064934aef1cb53286abcf763f5537a3862654
kirtibhave1000@gmail.com <kirtibhave1000@gmail.com>
2020-01-30 15:49:05 +0530

    ADDED:Function to calculate gambler won or loose

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,17 +1,30 @@
+#!/bin/bash 
 echo "**********Welcome to Gambling Simulator Problem**********"
 
 #CONSTANTS
 STAKE_PER_DAY=100
 BET_PER_GAME=1
 
-function checkCondition(){
-RandomCheck=$(($RANDOM%2))
-	if [ 1 -eq $RandomCheck ]
-	then
-		echo "Congratulations..!!You won"
-	else
-		echo "You loose..!!Better luck next time."
-	fi
-}
+#VARIABLES
+cash=$STAKE_PER_DAY
 
-checkCondition
+function calculatePercentage(){
+   MAXIMUM_RESULT=$(($STAKE_PER_DAY+(50*$STAKE_PER_DAY/100)))  
+   MINIMUM_RESULT=$(($STAKE_PER_DAY-(50*$STAKE_PER_DAY/100)))
+}
+calculatePercentage
+
+function calculateGambler(){
+while [[ $cash -gt $MINIMUM_RESULT && $cash -lt $MAXIMUM_RESULT ]]
+do
+RandomCheck=$(($RANDOM%2))
+	if [[ 1 -eq $RandomCheck ]]
+	then
+		cash=$(($cash+$BET_PER_GAME))
+	else
+		cash=$(($cash-$BET_PER_GAME))
+	fi
+	echo $cash
+done
+}
+calculateGambler
