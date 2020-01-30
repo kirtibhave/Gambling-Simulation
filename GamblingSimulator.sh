9a3ce0655b45900ea47ef28c00cee19cca4edee3
kirtibhave1000@gmail.com <kirtibhave1000@gmail.com>
2020-01-30 15:13:42 +0530

    ADDED:Checked whether gambler won or loose

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -4,3 +4,14 @@ echo "**********Welcome to Gambling Simulator Problem**********"
 STAKE_PER_DAY=100
 BET_PER_GAME=1
 
+function checkCondition(){
+RandomCheck=$(($RANDOM%2))
+	if [ 1 -eq $RandomCheck ]
+	then
+		echo "Congratulations..!!You won"
+	else
+		echo "You loose..!!Better luck next time."
+	fi
+}
+
+checkCondition
