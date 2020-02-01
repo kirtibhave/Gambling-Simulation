90c5394a1757228be495ec2a5cacc760048ffb7e
kirtibhave <kirtibhave1000@gmail.com>
2020-02-01 12:09:07 +0530

    ADDED: Function to check gambler won or loose

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -48,6 +48,24 @@ do
 done | sort -rn -k2
 }
 
+function playingForNextMonth(){
+read -p "enter your choice" choice
+echo "1 for Yes"
+echo "0 for No"
+if [[ $choice == 1 ]]
+then
+	echo "play for next month"
+	calculateTotalAmount
+	echo Luckiest day2 :: $(sorting | head -1 )
+	echo Unluckiest day2 :: $(sorting | tail -1 )
+else
+	echo "sorry!! you cannot play"
+fi
+}
+
 calculateTotalAmount
-echo Luckiest day:: $(sorting | head -1 )
-echo Unluckiest day:: $(sorting | tail -1 )
+echo Luckiest day2 :: $(sorting | head -1 )
+echo Unluckiest day2 :: $(sorting | tail -1 )
+playingForNextMonth
+
+
